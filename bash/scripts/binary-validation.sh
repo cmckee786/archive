a8a4fab4c0133111b35e1767cdcbb4f7ce0d3419
cmckee786 <mckee_christian@yahoo.com>
2025-08-19 17:42:49 -0400

    teehee
    shuffle stuff around
    
    unecessary arg pass

--- bash/scripts/binary-validation.sh
+++ bash/scripts/binary-validation.sh
@@ -21,52 +21,54 @@ json_setup() {
 	curl -L \
 		-H "Accept: application/vnd.github+json" \
 		-H "X-GitHub-Api-Version: 2022-11-28" \
-		https://api.github.com/repos/rust-lang/mdBook/releases > "${API_ARRAY}"
+		https://api.github.com/repos/rust-lang/mdBook/releases >"${API_ARRAY}"
 
 	JSON=$(jq "${JQUERY}" "${API_ARRAY}")
 }
 
 api_fetch() {
 
-	local DL_URL=
-	local API_DIGEST=
-	local ZIP=
-	local ZIP_DIGEST=
+	local DL_URL
+	local API_DIGEST
+	local ZIP
+	local ZIP_DIGEST
 
-	if [[ -n $1 ]]; then
+	printf "\nParsing API JSON return data and assigning variables...\n\n"
 
-		printf "\nParsing API JSON return data and assigning variables...\n\n"
+	DL_URL="$(jq -r '.[].browser_download_url' <<<"${JSON}")"
+	API_DIGEST="$(jq -r '.[].digest' <<<"${JSON}" | cut -d: -f2)"
+	ZIP="$(jq -r '.[].name' <<<"${JSON}")"
 
-		DL_URL="$(jq -r '.[].browser_download_url' <<< "${JSON}")"
-		API_DIGEST="$(jq -r '.[].digest' <<< "${JSON}" | cut -d: -f2)"
-		ZIP="$(jq -r '.[].name' <<< "${JSON}")"
+	printf "%3s %s\n" "URL:" "${DL_URL}" "API_DIGEST:" "${API_DIGEST}" "ZIP:" "${ZIP}"
+	printf "\nFetching binary...\nCalculating ZIP digest...\n\n"
 
-		printf "%3s %s\n" "URL:" "${DL_URL}" "API_DIGEST:" "${API_DIGEST}" "ZIP:" "${ZIP}"
-		printf "\nFetching binary...\nCalculating ZIP digest...\n\n"
+	curl -LO "$DL_URL"
+	ZIP_DIGEST=$(sha256sum "${PWD}/${ZIP}" | awk '{print $1}')
 
-		curl -LO "$DL_URL"
-		ZIP_DIGEST=$(sha256sum "${PWD}/${ZIP}" | awk '{print $1}')
+	printf "\nZIP_DIGEST: %s\n" "${ZIP_DIGEST}"
 
-		printf "\nZIP_DIGEST: %s\n" "${ZIP_DIGEST}"
-
-		if [[ "${API_DIGEST}" = "${ZIP_DIGEST}" ]]; then
-			printf "\nDigest check succeeded!"
-			tar xfz "${ZIP}" && rm -f "${ZIP}" "${JSON}"
-			printf "\nCleaning up...\nmdBook binary unzipped and ready for execution!\n"
-			exit 0
-		else
-			printf "\nThe API digest appears to be different than the downloaded binary digest:\n"
-			printf "%2s %s\n" "API sha:" "${API_DIGEST}" "ZIP sha:" "${ZIP_DIGEST}"
-			printf "\nDumping JSON and cleaning up...\n\n"
-			cat "${JSON}"
-			rm -f "${ZIP}" "${JSON}"
-			exit 1
-		fi
-
+	if [[ "${API_DIGEST}" = "${ZIP_DIGEST}" ]]; then
+		printf "\nDigest check succeeded!"
+		tar xfz "${ZIP}" && rm -f "${ZIP}" "${API_ARRAY}"
+		printf "\nCleaning up...\nmdBook binary unzipped and ready for execution!\n"
+		exit 0
 	else
-		printf "This script requires an mdBook version as an argument, for example 'binary-validation.sh v0.1.15'"
+		printf "\nThe API digest appears to be different than the downloaded binary digest:\n"
+		printf "%2s %s\n" "API sha:" "${API_DIGEST}" "ZIP sha:" "${ZIP_DIGEST}"
+		printf "\nDumping JSON and cleaning up...\n\n"
+		cat "${JSON}"
+		rm -f "${ZIP}" "${API_ARRAY}"
+		exit 1
 	fi
+
 }
 
-json_setup
-api_fetch "$@"
+if [[ -n $1 ]]; then
+
+	json_setup
+	api_fetch
+
+else
+	printf "This script requires an mdBook version as an argument, for example 'binary-validation.sh v0.1.15'"
+	exit 1
+fi
