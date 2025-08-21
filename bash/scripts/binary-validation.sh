b4efa5f24f8eed06bd5f40588a485e178071bf6e
cmckee786 <mckee_christian@yahoo.com>
2025-08-21 12:12:49 -0400

    significant refactor

--- bash/scripts/binary-validation.sh
+++ bash/scripts/binary-validation.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Targeting mdBook version v0.4.52 as of Aug, 2025
-# Only requests 10 latest release records, remove ?per_page=10 to fetch all
+# Only requests 5 latest release records, remove ?per_page=5 to fetch all
 # Requires mdBook semantic version input from command line
 # For example 'binary-validation.sh v0.1.15'
 
+set -euo pipefail
+
 MDBOOK_VERSION=$1
-API_ARRAY=$(mktemp)
-declare JSON
+declare json
 
 json_setup() {
 
-	local JQUERY="
+	local jquery="
 	map(select(.tag_name==\"${MDBOOK_VERSION}\"))
 	| .[].assets
 	| map(select(.name==\"mdbook-${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz\"))
@@ -19,64 +20,63 @@ json_setup() {
 	printf "mdBook Binary script executing..."
 	printf "\nQuerying GH API for JSON %s mdBook record..." "${MDBOOK_VERSION}"
 
-	curl -sL \
-		-H "Accept: application/vnd.github+json" \
-		-H "X-GitHub-Api-Version: 2022-11-28" \
-		https://api.github.com/repos/rust-lang/mdBook/releases?per_page=10 \
-		>"${API_ARRAY}" ||
+	json="$(gh api repos/rust-lang/mdBook/releases?per_page=5 --jq "${jquery}" ||
 		{
-			printf "\nSomething went wrong with the GH API request. Dumping curl error."
+			printf "\nSomething may have gone wrong with the GH API request."
 			exit 1
-		} 2>&1
-
-	JSON="$(jq "${JQUERY}" "${API_ARRAY}")" ||
-		{
-			printf "\nEncountered error processing JSON, version may not exist.
-			Dumping GH API JSON record if present:\n%s" "${JSON}"
-			exit 1
-		} 2>&1
+		} 1>&2)"
 }
 
 api_fetch() {
 
-	local DL_URL
-	local API_DIGEST
-	local ZIP
-	local ZIP_DIGEST
+	local dl_url
+	local api_digest
+	local zip
+	local zip_digest
 
-	printf "\nParsing API JSON return data and assigning variables...\n\n"
+	if [[ -n "${json}" ]]; then
+		printf "\nParsing API JSON return data and assigning variables...\n\n"
 
-	DL_URL="$(jq -r '.[].browser_download_url' <<<"${JSON}")"
-	API_DIGEST="$(jq -r '.[].digest' <<<"${JSON}" | cut -d: -f2)"
-	ZIP="$(jq -r '.[].name' <<<"${JSON}")"
+		dl_url="$(jq -r '.[].browser_download_url' <<<"${json}")"
+		api_digest="$(jq -r '.[].digest' <<<"${json}" | cut -d: -f2)"
+		zip="$(jq -r '.[].name' <<<"${json}")"
 
-	printf "%3s %s\n" "URL:" "${DL_URL}" "API_DIGEST:" "${API_DIGEST}" "ZIP:" "${ZIP}"
-	printf "\nFetching binary...\nCalculating ZIP digest...\n\n"
+		printf "%3s %s\n" "URL:" "${dl_url}" "API_DIGEST:" "${api_digest}" "ZIP:" "${zip}"
+		printf "\nFetching binary...\nCalculating ZIP digest...\n\n"
 
-	curl -LO --progress-bar "$DL_URL"
-	ZIP_DIGEST=$(sha256sum "${PWD}/${ZIP}" | awk '{print $1}')
+		curl -LO --progress-bar "$dl_url"
+		zip_digest=$(sha256sum "./${zip}" | awk '{print $1}')
 
-	printf "\nZIP_DIGEST: %s\n" "${ZIP_DIGEST}"
+		printf "\nZIP_DIGEST: %s\n" "${zip_digest}"
 
-	if [[ "${API_DIGEST}" = "${ZIP_DIGEST}" ]]; then
-		printf "\nDigest check succeeded!\nCleaning up...\nmdBook binary unzipped and ready for execution!"
-		tar xfz "${ZIP}" && rm -f "${ZIP}" && rm -f "${API_ARRAY}"
-		exit 0
+		if [[ "${api_digest}" = "${zip_digest}" ]]; then
+			printf "\nDigest check succeeded!\nCleaning up...\nmdBook binary unzipped and ready for execution!"
+			tar xfz "${zip}"
+			rm -f "${zip}"
+			exit 0
+		else
+			printf "\nThe API digest appears to be different than the downloaded binary digest:"
+			printf "\n%2s %s" "API sha:" "${api_digest}" "ZIP sha:" "${zip_digest}"
+			printf "\n\nDumping JSON record and cleaning up...\n\n"
+			rm -f "${zip}"
+			echo "${json}" | jq '.'
+			exit 1
+		fi
+
 	else
-		printf "\nThe API digest appears to be different than the downloaded binary digest:"
-		printf "\n%2s %s\n" "API sha:" "${API_DIGEST}" "ZIP sha:" "${ZIP_DIGEST}"
-		printf "\nDumping JSON record and cleaning up...\n\n"
-		cat "${JSON}" && rm -f "${ZIP}" && rm -f "${API_ARRAY}"
+		printf "\nThe processed JSON record appears to be empty, queried version may not exist...\nExiting..."
 		exit 1
 	fi
 }
 
-if [[ $1 =~ v([[:digit:]]+\.){2}[[:digit:]]+ && -n $1 ]]; then
+reg_pattern="^v([[:digit:]]+\.){2}[[:digit:]]+$"
+
+if [[ $1 =~ ${reg_pattern} && -n $1 ]]; then
 
 	json_setup
 	api_fetch
 
 else
-	printf "This script requires an mdBook version as an argument, for example 'binary-validation.sh v0.4.20'"
+	printf "This script requires an mdBook version as an argument and to observe mdBook's semantic versioning, for example 'binary-validation.sh v0.4.20'"
 	exit 1
 fi
