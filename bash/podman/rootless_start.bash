fa4ed901642d54b171703da6244548f05e6cae45
cmckee786 <mckee_christian@yahoo.com>
2025-10-20 16:51:55 -0400

    fix(podman): script now successfully builds
    - creates service account and necessary user keys for mgmt
    - creates service files to persist pod on host restarts
    - at this time the loginctl linger command will need to be manually
    called on host restarts

--- bash/podman/rootless_start.bash
+++ bash/podman/rootless_start.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#v 0.1.1
+#v 1.1.2
 # Authored by Christian McKee - cmckee786@github.com
-# Rootless pihole podman container pipeline
+# Rootless pihole podman container pipeline managed by
+# lingering service account
 
 # Allows use of "podman auto-update" to easily update
 # pod images based on this label: --label "io.containers.autoupdate=registry"
@@ -10,8 +11,10 @@
 
 # NOTE: Rootless containers cannot create network devices
 # so a tap device is made and NAT is implemented on the host
+# via slirp4netns
 
 # Requirements:
+# - SSH access to remote target with admin privileges
 # - Redirect all port 80/443 and 53 incoming traffic via firewall
 #   - Redirect or front facing reverse proxy is considered more secure
 #     than editing sys.ipv4.unprivileged_port_start
@@ -20,71 +23,103 @@
 # - Enable service unit
 
 
-pihole_start() {
-podman pod create \
-	--infra \
-	-p 8080:80 \
-	-p 8443:443 \
-	-p 10053:53 \
-	-p 10053:53/udp \
-	pod_pihole_rootless
+#NOTE: Change these global variables to match your environment
+
+USER=chris
+REMOTE_TARGET=10.0.0.182
+USER_PRIV_KEY="$HOME"/.ssh/raspi/rpi
+REMOTE_PRIV_KEY="$HOME"/.ssh/piholeserviceacc/pihole_stack
+REMOTE_KEY="$HOME"/.ssh/piholeserviceacc/pihole_stack.pub
+
+if [[ ! $(ssh ${USER}@rpi 'id piholeserviceacc') ]]; then
+	printf "Implementing pihole service account..."
+	ssh -F /dev/null -i "${USER_PRIV_KEY}" "${USER}"@"${REMOTE_TARGET}" "
+		sudo bash -c '
+		useradd -m -s /usr/bin/bash piholeserviceacc
+		passwd -d piholeserviceacc
+
+		mkdir -p /home/piholeserviceacc/.config/systemd/user/
+		mkdir -p /home/piholeserviceacc/pihole/data/etc
+		mkdir -p /home/piholeserviceacc/.ssh/
+
+		touch /home/piholeserviceacc/.ssh/authorized_keys
+		chown -R piholeserviceacc:piholeserviceacc /home/piholeserviceacc
+		chmod 700 /home/piholeserviceacc/.ssh
+		chmod 600 /home/piholeserviceacc/.ssh/authorized_keys
+		'"
+fi
+
+if [[ ! -d "$HOME"/.ssh/piholeserviceacc/ ]]; then
+	mkdir -p "$HOME"/.ssh/piholeserviceacc/
+	ssh-keygen -t ed25519 -a 32 -f "$HOME"/.ssh/piholeserviceacc/pihole_stack
+	scp -i "${USER_PRIV_KEY}" "${REMOTE_KEY}" "${USER}"@"${REMOTE_TARGET}":/tmp/
+	ssh -i "${USER_PRIV_KEY}" "${USER}"@"${REMOTE_TARGET}" '
+		cat /tmp/pihole_stack.pub | sudo tee -a /home/piholeserviceacc/.ssh/authorized_keys
+		'
+fi
+
+ssh \
+	-F /dev/null \
+	-i "${REMOTE_PRIV_KEY}" piholeserviceacc@"${REMOTE_TARGET}" \
+"podman pod create \
+    --infra \
+    --network=slirp4netns \
+    -p 8080:80 \
+    -p 8443:443 \
+    -p 10053:53 \
+    -p 10053:53/udp \
+    pod_pihole_rootless
 
 podman create \
-	--name cloudflared-cf \
-	--pod pod_pihole_rootless \
-	--label "io.containers.autoupdate=registry" \
-	-e TUNNEL_MANAGEMENT_DIAGNOSTICS=false \
-	docker.io/cloudflare/cloudflared:latest proxy-dns \
-		--address 0.0.0.0 \
-		--port 5353 \
-		--upstream https://1.1.1.1/dns-query \
-		--upstream https://1.0.0.1/dns-query
+    --name cloudflared-cf \
+    --pod pod_pihole_rootless \
+    --label 'io.containers.autoupdate=registry' \
+    -e TUNNEL_MANAGEMENT_DIAGNOSTICS=false \
+    docker.io/cloudflare/cloudflared:latest proxy-dns \
+        --address 0.0.0.0 \
+        --port 5353 \
+        --upstream https://1.1.1.1/dns-query \
+        --upstream https://1.0.0.1/dns-query
 
 podman create \
-	--name cloudflared-goog \
-	--pod pod_pihole_rootless \
-	--label "io.containers.autoupdate=registry" \
-	-e TUNNEL_MANAGEMENT_DIAGNOSTICS=false \
-	docker.io/cloudflare/cloudflared:latest proxy-dns \
-		--address 0.0.0.0 \
-		--port 5353 \
-		--upstream https://8.8.8.8/dns-query \
-		--upstream https://8.8.4.4/dns-query
+    --name cloudflared-goog \
+    --pod pod_pihole_rootless \
+    --label 'io.containers.autoupdate=registry' \
+    -e TUNNEL_MANAGEMENT_DIAGNOSTICS=false \
+    docker.io/cloudflare/cloudflared:latest proxy-dns \
+        --address 0.0.0.0 \
+        --port 5353 \
+        --upstream https://8.8.8.8/dns-query \
+        --upstream https://8.8.4.4/dns-query
 
 podman create \
-	--name pihole \
-	--pod pod_pihole_rootless \
-	--label "io.containers.autoupdate=registry" \
-	--cap-add SYS_NICE \
-	-e TZ='America/New_york' \
-	-e FTLCONF_webserver_api_password='change-me!' \
-	-e FTLCONF_dns_upstreams='cloudflared-cf#5353;cloudflared-goog#5353' \
-	-v /home/chris/pihole/data/etc:/etc/pihole/ \
-	docker.io/pihole/pihole:latest
+    --name pihole \
+    --pod pod_pihole_rootless \
+    --label 'io.containers.autoupdate=registry' \
+    --cap-add SYS_NICE \
+    -e TZ='America/New_York' \
+    -e FTLCONF_webserver_api_password='change-me!' \
+    -e FTLCONF_dns_upstreams='cloudflared-cf#5353;cloudflared-goog#5353' \
+    -v /home/piholeserviceacc/pihole/data/etc:/etc/pihole/ \
+    docker.io/pihole/pihole:latest
 
 podman pod start pod_pihole_rootless
-}
+podman generate systemd --new -nf pod_pihole_rootless
+mv *.service /home/piholeserviceacc/.config/systemd/user/
+systemctl --user enable pod-pod_pihole_rootless
+loginctl enable-linger piholeserviceacc
+"
 
-#TODO:
-  # - Test generation of systemd unit
-  # - Test implementation of service account
-  # - Test for necessary command binaries, install dependencies if absent
-init() {
-	local service_dir="$HOME"/.config/system/user/
-
-	if [[ ! $(id piholeserviceacc) ]]; then
-		sudo useradd -s /usr/bin/bash piholeserviceacc
-	fi
-
-	if [[ ! -d $service_dir ]]; then
-		mkdir -p "$service_dir" && cd "$service_dir"
-	fi
-
-	pihole_start
-	podman generate systemd --new -nf pod_pihole_rootless
-	# sed -i /User=piholeserviceacc/ pod-pod_pihole_rootless
-	systemctl --user enable --now pod-pod_pihole_rootless
-}
-
-init
-
+#NOTE: From here a decision should be made whether to redirect traffic
+# or modify privileged ports after verification of successful start of pod
+#
+#WARN: To restart from zero:
+# From remote host:
+# 	podman pod rm -f pod_pihole_rootless
+# 	rm /home/piholeserviceacc/.config/systemd/user/*.service
+# 	sudo rm -rf /home/piholeserviceacc/
+#	sudo userdel piholeserviceacc
+# From user host and account:
+#	rm -rf /home/{USER}/.ssh/piholeserviceacc/
+# The script will then rebuild from scratch and create new keys, service files
+# and pod
