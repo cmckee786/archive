6bc7f03da229d417879798e1ac21210d0d8145a8
cmckee786 <mckee_christian@yahoo.com>
2026-02-13 17:23:30 -0500

    feat: add fzf install script

---
+++ bash/scripts/fzfinstall.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Show a fuzzy-finder TUI for picking new packages to install.
+
+fzf_args=(
+  --multi
+  # {1} is important here so that package manager does not get info for each package
+  # Only searches info of the currently selected package
+  --preview 'zypper info {1}' # apt show, dnf info
+  --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select'
+  --preview-label-pos='bottom'
+  --preview-window 'down:65%:wrap'
+  --bind 'alt-p:toggle-preview'
+  --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
+  --bind 'alt-k:preview-up,alt-j:preview-down'
+  --color 'pointer:green,marker:green'
+)
+
+# dnf repoquery --all --available --qf "%{name}" | sort -u | fzf
+# apt-cache pkgnames | fzf
+
+pkg_names=$(zypper --quiet search -t package "" | awk -F'|' 'NR>2 {print $2}' | tr -d ' ' | fzf "${fzf_args[@]}")
+# pkg_names=$(pacman -Slq | fzf "${fzf_args[@]}")
+#
+if [[ -n "$pkg_names" ]]; then
+  # Convert newline-separated selections to space-separated
+  echo "$pkg_names" | tr '\n' ' ' | xargs sudo zypper in
+fi
