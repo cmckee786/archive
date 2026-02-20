c7a26d46f23f0ea19d7e6e63e477e4c4e808ccb8
cmckee786 <mckee_christian@yahoo.com>
2026-02-20 15:32:00 -0500

    fix: 4 space tabs, reformat

--- bash/scripts/fzfinstall.bash
+++ bash/scripts/fzfinstall.bash
@@ -1,17 +1,16 @@
 #!/bin/bash
-
 # Show a fuzzy-finder TUI for picking new packages to install.
 
 fzf_args=(
-  --multi
-  --preview '[[ -n "{1}" ]] && zypper info {1}' # apt show, dnf info
-  --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select'
-  --preview-label-pos='bottom'
-  --preview-window 'down:65%:wrap'
-  --bind 'alt-p:toggle-preview'
-  --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
-  --bind 'alt-k:preview-up,alt-j:preview-down'
-  --color 'pointer:green,marker:green'
+    --multi
+    --preview '[[ -n "{1}" ]] && zypper info {1}' # apt show, dnf info, pacman -Sii
+    --preview-label='alt-p: toggle description, alt-j/k: scroll, tab: multi-select'
+    --preview-label-pos='bottom'
+    --preview-window 'down:65%:wrap'
+    --bind 'alt-p:toggle-preview'
+    --bind 'alt-d:preview-half-page-down,alt-u:preview-half-page-up'
+    --bind 'alt-k:preview-up,alt-j:preview-down'
+    --color 'pointer:green,marker:green'
 )
 
 # dnf repoquery --all --available --qf "%{name}" | sort -u | fzf
@@ -20,8 +19,7 @@ fzf_args=(
 
 pkg_names=$(zypper --quiet search -t package "" | awk -F'|' '/^i/ || /^ / {print $2}' | tr -d ' ' | fzf "${fzf_args[@]}")
 
-
 if [[ -n "$pkg_names" ]]; then
-  # Convert newline-separated selections to space-separated
-  echo "$pkg_names" | tr '\n' ' ' | xargs sudo zypper in
+    # Convert newline-separated selections to space-separated
+    echo "$pkg_names" | tr '\n' ' ' | xargs sudo zypper in
 fi
--- bash/scripts/keep-active.sh
+++ bash/scripts/keep-active.sh
@@ -4,14 +4,14 @@ declare -a PATHS=(home about join certify verify)
 declare CURL_REQ_STRING=
 declare BASE_URL=https://prolug.org/
 
-function keep_warm {
+keep_warm() {
 
-	for item in "${PATHS[@]}"; do
-		CURL_REQ_STRING+="${BASE_URL}${item} "
-	done
+    for item in "${PATHS[@]}"; do
+        CURL_REQ_STRING+="${BASE_URL}${item} "
+    done
 
-	curl -sL --rate 60/m --retry 3 --retry-delay 2 ${CURL_REQ_STRING} >/dev/null
-	exit 0
+    curl -sL --rate 60/m --retry 3 --retry-delay 2 ${CURL_REQ_STRING} >/dev/null
+    exit 0
 }
 
 keep_warm
