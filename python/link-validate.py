0217b6b0167f0219367c4b712a63dacaab764722
cmckee786 <mckee_christian@yahoo.com>
2026-03-27 14:52:21 -0400

    fix: update user-agent

--- python/link-validate.py
+++ python/link-validate.py
@@ -138,7 +138,7 @@ def validate_link(matched_item: dict[str, str | int | Path]) -> tuple:
     """
     headers: dict = {
         "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:149.0) Gecko/20100101 Firefox/149.0"
         ),
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
